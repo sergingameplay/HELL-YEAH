a1b1dce751a359053104950d00ab47ba3b0b62a5
sergingameplay <sergiootavio350@gmail.com>
2025-05-07 08:03:22 -0300

    Add files via upload

---
+++ HELL-YEAH-main/js/main.js
@@ -0,0 +1,41 @@
+function muda_cor(){
+
+    let body = document.getElementById("bg");
+    // número de 0 até 10
+    let x = Math.floor(Math.random() * 11)
+
+    switch(x){
+        case 0:
+            body.style.backgroundColor = "#3d828b";
+            break;
+        case 1:
+            body.style.backgroundColor = "#045083";
+            break;
+        case 2:
+            body.style.backgroundColor = "#4F4F4F";
+            break;
+        case 3:
+            body.style.backgroundColor = "#6959CD";
+            break;
+        case 4:
+            body.style.backgroundColor = "#4169E1";
+            break;
+        case 5:
+            body.style.backgroundColor = "#008B8B";
+            break;
+        case 6:
+            body.style.backgroundColor = "#DC143C";
+            break;
+        case 7:
+            body.style.backgroundColor = "#FF4500";
+            break;
+        case 8:
+            body.style.backgroundColor = "#FFD700";
+            break;
+        case 9:
+            body.style.backgroundColor = "#FFFF00";
+            break;
+        default:
+            body.style.backgroundColor = "#000000";
+    }
+}
